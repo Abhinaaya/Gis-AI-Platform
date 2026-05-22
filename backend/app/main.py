42963151efabf9640113a05f953b0ac72dadf695
Abhinaaya <nunna.abhinaya@students.iiit.ac.in>
2026-05-22 11:39:22 +0530

    Add Geojson endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import geopandas as gpd
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "GIS AI Platform Running"}
+
+@app.get("/geo-data")
+def get_geo_data():
+
+    gdf = gpd.read_file("../data/sample.geojson")
+    import json
+
+    return json.loads(gdf.to_json())
+
